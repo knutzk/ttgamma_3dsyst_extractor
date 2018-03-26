a427702e9f8677f87e3e5f57cfb08e9791dba079
Knut Zoch <knut.zoch@gmail.com>
2018-03-26 17:28:33 +0200

    Further cleanup and addition of documentation

--- util/output_hists.hh
+++ util/output_hists.hh
@@ -1,15 +1,40 @@
 #ifndef _OUTPUT_HISTS_HH_
 #define _OUTPUT_HISTS_HH_
 
-#include <iostream>
 #include <memory>
-#include <sstream>
 #include <string>
 #include <vector>
 
-#include "TFile.h"
 #include "TH1F.h"
 #include "TH3F.h"
+
+class TFile;
+
+class SystHist1D : public TH1F {
+public:
+  //! Initialise the 1D systematic histogram, with the path to
+  //! the input file as input parameter.
+  SystHist1D(const std::string& file_path);
+
+  //! Default destructor.
+  virtual ~SystHist1D() = default;
+
+  //! Set the internal pointer for the container that contains
+  //! strings of all MC histograms.
+  void setMCHists(std::vector<std::string>* hists);
+
+  //! Create the data/MC ratio from the input file and fill the
+  //! histogram based on that ratio.
+  void fillFromRatios();
+
+private:
+  //! Path to the input file.
+  std::string file_path_;
+
+  //! Pointer to the container with MC histogram names.
+  std::vector<std::string>* mc_hists_;
+};
+
 
 //! Initialise the main output object (of class TH3F).
 std::unique_ptr<TH3F> initOutput3DHist(const std::string& base_name);
@@ -20,19 +45,5 @@ void fill3D(TH3F* h_3D,
             const std::vector<std::string>& pt_slice_strings,
             std::vector<std::string>* mc_hists,
             TFile* output_file);
-
-
-class SystHist1D : public TH1F {
-public:
-  SystHist1D(const std::string& file_path);
-  virtual ~SystHist1D() = default;
-
-  void setMCHists(std::vector<std::string>* hists);
-  void fillFromRatios();
-
-private:
-  std::string file_path_;
-  std::vector<std::string>* mc_hists_;
-};
 
 #endif  // _OUTPUT_HISTS_HH_
